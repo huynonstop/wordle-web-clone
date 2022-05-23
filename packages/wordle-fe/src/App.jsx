afa4174a639de39a79fe4e7250b8babea0959897
huynonstop <huynonstop123nt@gmail.com>
2022-05-23 08:26:35 +0700

    init fe

--- packages/wordle-fe/src/App.jsx
+++ packages/wordle-fe/src/App.jsx
@@ -1,49 +1,9 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { app, appHeader } from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((preCount) => preCount + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.jsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className={app}>
+      <h1 className={appHeader}>(Hello) Wordle</h1>
     </div>
   );
 }
