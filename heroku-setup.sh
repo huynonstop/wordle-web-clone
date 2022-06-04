d4af36e51030abaaaa486e83060ceeda95333251
huynonstop <huynonstop123nt@gmail.com>
2022-06-04 11:57:11 +0700

    add nlf

--- heroku-setup.sh
+++ heroku-setup.sh
@@ -1,8 +1,9 @@
 heroku login
 
 heroku create wordle-be-api
-https://git.heroku.com/wordle-be-api.git
+https://git.heroku.com/wordle-be-api.git https://wordle-be-api.herokuapp.com/
 git remote add wordle-be-api https://git.heroku.com/wordle-be-api.git
+git push wordle-be-api master
 
 heroku buildpacks:add -a wordle-be-api  heroku/nodejs
 heroku buildpacks:add -a wordle-be-api  https://github.com/lstoll/heroku-buildpack-monorepo -i 1
